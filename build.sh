5b1994a4c92f25e20d67fdf77c21ff6e96fa0226
Ahmad Gharib <12767433+aygharib@users.noreply.github.com>
2022-12-21 16:44:35 -0500

    setup project with glfw dependency

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+set -x
+
+rm -rf build
+mkdir build
+pushd build
+
+conan install .. --build=missing -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True
+cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake --build .
+bin/openglbyexample
