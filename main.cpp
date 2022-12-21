829a6571289bce628a8c79153bf91ef7f8fe0c05
Ahmad Gharib <12767433+aygharib@users.noreply.github.com>
2022-12-21 17:11:18 -0500

    empty window is rendered

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
+#include <chrono>
 #include <iostream>
+#include <thread>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 int main() {
+    glfwInit();
+    GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
     std::cout << "Hello world\n";
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
     return 0;
 }
