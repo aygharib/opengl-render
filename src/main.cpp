0bc988980f8fdc8ab3db1943da969da6cdcf596e
Ahmad Gharib <12767433+aygharib@users.noreply.github.com>
2022-12-28 17:53:12 -0500

    Hello Window: Blank screen render

--- src/main.cpp
+++ src/main.cpp
@@ -4,89 +4,35 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+constexpr int WINDOW_WIDTH    = 800;
+constexpr int WINDOW_HEIGHT   = 600;
+constexpr char WINDOW_NAME[6] = { 'R', 'e', 'n', 'd', 'e', 'r' };
 
-#include "shaderclass.h"
-#include "camera.h"
-
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-
-float lastX = 400, lastY = 300;
-bool firstMouse = true;
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
-
-glm::vec3 cubePositions[] = {
-    glm::vec3( 0.0f,  0.0f,  0.0f), 
-    glm::vec3( 2.0f,  5.0f, -15.0f), 
-    glm::vec3(-1.5f, -2.2f, -2.5f),  
-    glm::vec3(-3.8f, -2.0f, -12.3f),  
-    glm::vec3( 2.4f, -0.4f, -3.5f),  
-    glm::vec3(-1.7f,  3.0f, -7.5f),  
-    glm::vec3( 1.3f, -2.0f, -2.5f),  
-    glm::vec3( 1.5f,  2.0f, -2.5f), 
-    glm::vec3( 1.5f,  0.2f, -1.5f), 
-    glm::vec3(-1.3f,  1.0f, -1.5f)  
-};
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-    glViewport(0, 0, width, height);
-}
-
-void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
-    if (firstMouse) {
-        lastX = xpos;
-        lastY = ypos;
-        firstMouse = false;
-    }
-  
-    float xoffset = xpos - lastX;
-    float yoffset = lastY - ypos; 
-    lastX = xpos;
-    lastY = ypos;
-
-    camera.ProcessMouseMovement(xoffset, yoffset);
-}
-
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
-    camera.ProcessMouseScroll(static_cast<float>(yoffset));
-}
-
-void processInput(GLFWwindow *window, float deltaTime) {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-    if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS)
-        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS)
-        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-    
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        camera.ProcessKeyboard(FORWARD, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        camera.ProcessKeyboard(BACKWARD, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        camera.ProcessKeyboard(LEFT, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        camera.ProcessKeyboard(RIGHT, deltaTime);
-}
-
+// GLFW library must be initialized before most GLFW functions can be used
 auto initGLFW() -> void {
-    // Initialize GLFW, set core-profile and OpenGL version
     glfwInit();
+
+    // OpenGL version 3.3 as it's the first version thats considered "Modern OpenGL"
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    // Use Core Profile to remove deprecated features
+    // Remove backwards compatibility from older OpenGL versions
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 }
 
 // Create GLFW window and set it as current context
-auto createWindow() -> GLFWwindow* {
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+// GLFW window required
+auto createGLFWWindow() -> GLFWwindow* {
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME,
+        NULL, NULL);
+
     if (window == NULL) {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
     }
+
+    // Context is set to created window
     glfwMakeContextCurrent(window);
 
     return window;
@@ -99,204 +45,52 @@ auto initGLAD() -> void {
     }
 }
 
-void setCallbackFunctions(GLFWwindow* window) {
-    // Set callback function when resizing window
+// Update the size of rendering window (viewport) to match new GLFWwindow size
+// so OpenGL knows how to display data and coordinates with respect to the window
+auto framebuffer_size_callback(GLFWwindow* window, int width, int height) -> void {
+    glViewport(0, 0, width, height);
+}
+
+auto setCallbackFunctions(GLFWwindow* window) -> void {
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    // hide the cursor and capture it
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-
-    // Set mouse callback
-    glfwSetCursorPosCallback(window, mouse_callback);
-
-    // Set scroll callback
-    glfwSetScrollCallback(window, scroll_callback); 
 }
 
-auto createVAO() -> unsigned int {
-    // Create VBO and VAO
-    // Input
-    float vertices[] = {
-    // position           // texture
-    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-
-    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-
-    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-    };
-
-    unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 3,   // first triangle
-        1, 2, 3    // second triangle
-    };
-
-    // Vertex Buffer object
-    unsigned int VAO;
-    unsigned int VBO;
-    unsigned int EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-
-    // Bind Vertex Array Object
-    glBindVertexArray(VAO);
-    // Copy vertices array in a buffer for OpenGL to use
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    // texture attribute
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(2);
+auto processWindowInput(GLFWwindow *window) -> void {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
     
-    return VAO;
-}
-
-void render(Shader& shaderProgram, unsigned int vao, unsigned int texture1, unsigned int texture2) {
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-    // clear depth buffer before each render to clear previous depth data (similar to clearing previous color buffer)
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    // bind textures on corresponding texture units
-    glActiveTexture(GL_TEXTURE0); // activate the texture unit first before binding texture
-    glBindTexture(GL_TEXTURE_2D, texture1);
-    glActiveTexture(GL_TEXTURE1); // activate the texture unit first before binding texture
-    glBindTexture(GL_TEXTURE_2D, texture2);
-
-    shaderProgram.use();
-
-    // create View matrix
-    glm::mat4 view = camera.GetViewMatrix();
-
-    // create Projection matrix
-    glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), 800.0f / 600.0f, 0.1f, 100.0f);
-
-    /// Send matrices to shader (this is usually done each frame since transformation matrices tend to change a lot)
-    shaderProgram.setMat4("view", view);
-    shaderProgram.setMat4("projection", projection);
-
-    // Draw square
-    glBindVertexArray(vao);
-    for(unsigned int i = 0; i < 10; i++) {
-        // create Model matrix
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::translate(model, cubePositions[i]);
-        float angle = 20.0f * i; 
-        model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-        shaderProgram.setMat4("model", model);
-
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-    }
-}
-
-auto createTexture(const char* path, bool isPNG) -> unsigned int {
-    unsigned int texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-
-    // set the texture wrapping/filtering options (on the currently bound texture object)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    int width, height, nrChannels;
-    unsigned char* data = stbi_load(path, &width, &height, &nrChannels, 0);
-    if (data) {
-        if (isPNG) {
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-        } else {
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        }
-        glGenerateMipmap(GL_TEXTURE_2D);
-    } else {
-        std::cout << "Failed to load texture" << std::endl;
-    }
-    stbi_image_free(data);
-    std::cout << path << " loaded\n";
-    return texture;
+    if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 }
 
 int main() {
     initGLFW();
-    auto window = createWindow();
+    auto window = createGLFWWindow();
     initGLAD();
     setCallbackFunctions(window);
 
-    Shader shaderProgram("/home/wumbo/dev/opengl-by-example/shaders/shader.vs", "/home/wumbo/dev/opengl-by-example/shaders/shader.fs");
-    auto vao = createVAO();
-
-    stbi_set_flip_vertically_on_load(true);
-    unsigned int texture1 = createTexture("/home/wumbo/dev/opengl-by-example/resources/textures/container.jpg", false);
-    unsigned int texture2 = createTexture("/home/wumbo/dev/opengl-by-example/resources/textures/awesomeface.png", true);
-
-    // tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
-    shaderProgram.use();
-    shaderProgram.setInt("texture1", 0); // set it manually
-    shaderProgram.setInt("texture2", 1); // or with shader class
-
-    glEnable(GL_DEPTH_TEST);
-
-    float deltaTime = 0.0f;	// Time between current frame and last frame
-    float lastFrame = 0.0f; // Time of last frame
-
     while(!glfwWindowShouldClose(window)) {
-        float currentFrame = glfwGetTime();
-        deltaTime = currentFrame - lastFrame;
-        lastFrame = currentFrame;
+        processWindowInput(window);
 
-        processInput(window, deltaTime);
-        
-        render(shaderProgram, vao, texture1, texture2);
+        // Overwrites previous render screen with blank color
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-        // GLFW swap buffers and poll IO events
+        // Swap front and back GLFW color buffers after render commands are complete
+        // so the image is displayed without still being rendered to, to remove any
+        // render artifacts
         glfwSwapBuffers(window);
-        glfwPollEvents();    
+
+        // Poll input-output events (keyboard, mouse, etc) to update window state and
+        // call corresponding registered callback functions
+        glfwPollEvents();
     }
 
-    // Clean all GLFW resources that were allocated
+    // Before application terminates, GLFW should be terminated to free any resources
+    // allocated during or after GLFW initialization
     glfwTerminate();
+    
     return 0;
 }
